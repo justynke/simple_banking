9471b7f10aaf3b145a0515c689171b64b1c0a40a
justynke <j.dabrowska@gmx.de>
2020-12-29 11:23:07 +0100

    rewrite the app to use the class Account

--- banking.py
+++ banking.py
@@ -1,9 +1,30 @@
 from random import randint
 
+class Account:
+    def __init__(self, number, pin):
+        self.number = number
+        self.pin = pin
+
+def control_number(tab):
+    sum = 8
+    for i in range (6, 15):
+        if i % 2 == 0:
+            sum += tab[i] * 2
+            if tab[i] * 2 > 9:
+                sum -= 9
+        else:
+            sum += tab[i]
+
+    if sum % 10:
+        return 10 - sum % 10
+    else:
+        return 0
+
 def generate_card_number():
     card_num = [4,0,0,0,0,0]
-    for i in range(6, 16):
+    for i in range(6, 15):
         card_num.append(randint(0,9))
+    control_number(card_num)
     return card_num
 
 def generate_pin():
@@ -31,10 +52,6 @@ def user_menu():
             print("Bye!")
             exit()
 
-# Write your code here
-
-
-
 data = []
 
 
@@ -44,14 +61,13 @@ while True:
     print("0. Exit")
     choose = int(input())
     if choose == 1:
-        data.append(generate_card_number())
-        data.append(generate_pin())
+        data.append(Account(generate_card_number(), generate_pin()))
         print("Your card has been created")
         print("Your card number:")
-        print_number(data[-2])
+        print_number(data[-1].number)
         print()
         print("Your card PIN:")
-        print_number(data[-1])
+        print_number(data[-1].pin)
         print()
     elif choose == 2:
         print("Enter your card number:")
